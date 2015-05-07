8eec9dafeea56187a241fa784d0724dba550d95f
angelinachoi <angelinac@monarch.local>
2015-05-07 09:26:30 -0400

    Initial Commit

---
+++ QR2GS1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR2GS1
+//
+//  Created by Angelina Choi on 2015-05-07.
+//  Copyright (c) 2015 Angelina Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
